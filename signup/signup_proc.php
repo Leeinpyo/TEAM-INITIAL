d06623406373430fc77bd63c3e5f63e24cdb6113
Lee_Inpyo <0dldlsvy0@naver.com>
2019-05-26 15:53:12 +0900

    Update signup_proc.php
    관리자 사칭 방지 금지어 지정
    ( 관리자 운영자 admin )

--- signup/signup_proc.php
+++ signup/signup_proc.php
@@ -13,22 +13,31 @@ $email = $_POST['email'];
 
 echo "Send to database <br><b>ID :</b>: ".$id."<br><b>PASSWORD :</b>: ".$pass."<br><b>NAME :</b>: ".$name."<br><b>AGE :</b>: ".$age."<br><b>MON :</b>: ".$mon."<br><b>DAY :</b>: ".$day."<br><b>GENDER :</b>: ".$gender."<br><b>TEL :</b>: ".$tel."<br><b>EMAIL :</b>: ".$email."<br>";
 
-if($id && $pass && $name && $age && $mon && $day && $tel && $email)
-{
-  if(mysqli_num_rows(mysqli_query($db,"SELECT * FROM `member` WHERE id = '$id';"))==0)
+if ( $id=='관리자' || $name=='관리자' || $id=='admin' || $name=='admin' || $name=='운영자' || $id=='운영자' ) {
+  echo "<script>alert('관리자 사칭 방지로 인해 사용 할 수 없습니다'); history.back(-1); </script>"; // 중복된 아이디 등록 차단
+}
+
+
+if ( $id!='관리자' && $name!='관리자' && $id!='admin' && $name!='admin' && $name!='운영자' && $id!='운영자' ) {
+  
+  if($id && $pass && $name && $age && $mon && $day && $tel && $email)
   {
-    $sql = mysqli_query($db,"INSERT INTO `member` (`id`, `pass`, `name`, `age`, `mon`, `day`, `gender`, `tel`, `email`) VALUES ('$id',password('$pass'),'$name','$age','$mon','$day','$gender','$tel','$email');");
-    echo "<script>alert('등록 되었습니다'); location.href='../signin/signin.php'; </script>"; //등록 통과
+    if(mysqli_num_rows(mysqli_query($db,"SELECT * FROM `member` WHERE id = '$id';"))==0)
+    {
+      $sql = mysqli_query($db,"INSERT INTO `member` (`id`, `pass`, `name`, `age`, `mon`, `day`, `gender`, `tel`, `email`) VALUES ('$id',password('$pass'),'$name','$age','$mon','$day','$gender','$tel','$email');");
+      echo "<script>alert('등록 되었습니다'); location.href='../signin/signin.php'; </script>"; //등록 통과
+    }
+    else
+    {
+      echo "<script>alert('이미 등록된 아이디 입니다'); history.back(-1); </script>"; // 중복된 아이디 등록 차단
+    }
   }
   else
   {
-    echo "<script>alert('이미 등록된 아이디 입니다'); history.back(-1); </script>"; // 중복된 아이디 등록 차단
+    echo "<script>alert('잘못된 접근입니다. 관리자에게 문의해주세요.'); </script> <b><br><br>=== ACCESS DENIED ===</b>"; //기타 부정한 방법으로써의 접근 차단 및 쓸모없는 데이터가 저장되는 것을 방지함
   }
 }
-else
-{
-  echo "<script>alert('잘못된 접근입니다. 관리자에게 문의해주세요.'); </script> <b><br><br>=== ACCESS DENIED ===</b>"; //기타 부정한 방법으로써의 접근 차단 및 쓸모없는 데이터가 저장되는 것을 방지함
-}
+
 
 
 ?>
